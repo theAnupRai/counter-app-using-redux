d1678bb4ff9bb3f3b6a5999918fd1677a873eef2
theAnupRai <theanuprai592@gmail.com>
2023-04-14 16:15:40 +0530

    counter app using redux

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import Counter from './components/Counter'
+
+function App() {
+
+  return (
+    <div className="App">
+      <Counter />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Counter.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { decrement, increment } from '../redux/slices/CounterSlice';
+
+const Counter = () => {
+
+    const count = useSelector( (state) => state.counter.value);
+    const dispatch = useDispatch();
+
+  return (
+    <div>
+        <button onClick={() => dispatch(increment())}>
+            Increment
+        </button>
+        
+        <div>
+            {count}
+        </div>
+
+        <button onClick={() => dispatch(decrement())}>
+            Decrement
+        </button>
+    </div>
+  )
+}
+
+export default Counter
